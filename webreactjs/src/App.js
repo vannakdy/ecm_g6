0b8b260799417896b021c30f41bbe327d4597643
vannakdy <97593080+vannakdy@users.noreply.github.com>
2023-12-01 22:54:28 +0700

    Reactjs

---
+++ webreactjs/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Hello ReactJs Nodejs Exprssjs and MySQL databaserver</h1>
+        <button>Hello</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
